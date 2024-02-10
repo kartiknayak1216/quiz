d23a24be85f351f23ce54567d94c639a0acde9d3
kartiknayak1216 <nayakkartik1216@gmail.com>
2024-02-11 03:34:12 +0530

    Describe your changes here

--- api/index.js
+++ api/index.js
@@ -5,13 +5,21 @@ import connect from "./db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import Auth from "./routes/Auth.route.js";
+import path from "path";
+
 dotenv.config();
+const __dirname = path.resolve();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 // routes
 app.use("/api/auth", Auth);
 
