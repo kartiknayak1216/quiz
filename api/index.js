3e03e92f815b1cf2486bbc53563e88c8681633a8
kartiknayak1216 <nayakkartik1216@gmail.com>
2024-02-11 10:24:20 +0530

    deployment1

--- api/index.js
+++ api/index.js
@@ -5,22 +5,15 @@ import connect from "./db.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import Auth from "./routes/Auth.route.js";
-import path from "path";
 
 dotenv.config();
-const __dirname = path.resolve();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 const port = process.env.PORT || 3000;
 
-const clientPath = path.join(__dirname, "client");
-app.use(express.static(clientPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(clientPath, "index.html"));
-});
 // routes
 app.use("/api/auth", Auth);
 
